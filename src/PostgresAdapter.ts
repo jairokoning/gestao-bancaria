70f868d9350d379e873e6bee48ed70b28b9b8e73
Jairo Koning <jairo.koning@gmail.com>
2023-10-06 08:36:30 -0300

    feat: add postgres connection

---
+++ src/PostgresAdapter.ts
@@ -0,0 +1,19 @@
+import Connection from "./Connection";
+import pgp from "pg-promise";
+
+export default class PostgresAdapter implements Connection {
+	connection: any;
+
+	constructor () {
+		this.connection = pgp()("postgres://docker:123456@database:5432/app");
+	}
+
+	query(statement: string, params: any): Promise<any> {
+		return this.connection.query(statement, params);
+	}
+
+	close(): Promise<any> {
+		return this.connection.$pool.end();
+	}
+
+}
