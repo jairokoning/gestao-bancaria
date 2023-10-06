daa3b313ac2283a4e76e56f7e3f8af711b5b9eac
Jairo Koning <jairo.koning@gmail.com>
2023-10-06 19:55:06 -0300

    feat(ContaDAOPostgres): add function buscarConta and slq query

--- src/ContaDAOPostgres.ts
+++ src/ContaDAOPostgres.ts
@@ -4,9 +4,14 @@ import ContaDAO from "./ContaDAO";
 
 export default class ContaDAOPostgres implements ContaDAO {
 
-	constructor (readonly connection: Connection) {}
+	constructor (readonly connection: Connection) {}  
 
   async criarConta(conta: Conta): Promise<void> {
-    await this.connection.query("insert into gestao_bancaria.conta (conta_id, valor) values ($1, $2)", [conta.conta_id, conta.valor]);		    
+    await this.connection.query("INSERT INTO gestao_bancaria.conta (conta_id, valor) VALUES ($1, $2)", [conta.conta_id, conta.valor]);		    
+  }
+
+  async buscarConta(conta_id: number): Promise<any> {
+    const conta = await this.connection.query("SELECT * FROM gestao_bancaria.conta WHERE conta_id = $1", [conta_id]);
+    return conta
   }
 }
