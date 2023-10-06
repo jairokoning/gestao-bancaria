bfc618c368387c18a802da800c1847929ed368be
Jairo Koning <jairo.koning@gmail.com>
2023-10-06 20:03:35 -0300

    test: create api integration tests for transactions

--- tests/CriarTransacao.test.ts
+++ tests/CriarTransacao.test.ts
@@ -1,15 +1,18 @@
 import TransacaoDAOMemory from '../src/TransacaoDAOMemory'
 import ContaDAOMemory from '../src/ContaDAOMemory'
 import CriarTransacao from '../src/CriarTransacao'
-import Conta from '../src/Conta'
 import { IFormaPagamento } from '../src/Transacao'
 
-test("Deve criar uma nova transacao com forma de pagamento PIX", async () => {
-  const contaDAO = new ContaDAOMemory()
-  await contaDAO.criarConta({ conta_id: 1991, valor: 500 } as Conta)
-  const transacaoDAO = new TransacaoDAOMemory()
-  const transacao = new CriarTransacao(transacaoDAO, contaDAO)
-  const output = await transacao.execute("P" as unknown as IFormaPagamento, 1991, 25)
-  expect(output.saldo).toBe(475)
-  expect(output.conta_id).toBe(1991)
+
+
+describe("Test CriarTransacao usecase", () => {
+  test("Deve criar uma nova transacao com forma de pagamento PIX", async () => {
+    const contaDAO = new ContaDAOMemory()
+    await contaDAO.criarConta({ conta_id: 1991, valor: 500 })
+    const transacaoDAO = new TransacaoDAOMemory()
+    const transacao = new CriarTransacao(transacaoDAO, contaDAO)
+    const output = await transacao.execute("P" as unknown as IFormaPagamento, 1991, 25)
+    expect(output.saldo).toBe(475)
+    expect(output.conta_id).toBe(1991)
+  })
 })
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,12 +1,53 @@
 import axios from "axios"
 
-test("Deve criar uma nova conta bancária com saldo inicial de R$500", async () => {
-  const response = await axios.post("http://localhost:3333/conta", {
-    data: {
-      conta_id: 4477,
-      valor: 500
-    }
+import pgp from 'pg-promise'
+
+async function resetDatabase() {
+  const connection = pgp()("postgres://docker:123456@localhost:5432/app")
+	await connection.query("DROP TABLE IF EXISTS gestao_bancaria.conta")
+  await connection.query("DROP TABLE IF EXISTS gestao_bancaria.transacoes")  
+  await connection.query(`
+    CREATE TABLE gestao_bancaria.conta (
+      conta_id integer,
+      valor numeric
+    )
+  `)
+  await connection.query(`
+    CREATE TABLE gestao_bancaria.transacoes (
+      forma_pagamento char(1),
+      conta_id integer,
+      valor numeric
+    )
+  `)   		  
+	await connection.$pool.end()
+}
+
+describe('Testar API (main)', () => {
+  beforeAll(async () => {
+    await resetDatabase()
   })
-  const output = response.data
-  expect(output.saldo).toBe(500)
+
+  test("Deve criar uma nova conta bancária com saldo inicial de R$500", async () => {
+    const response = await axios.post("http://localhost:3333/conta", {
+      data: {
+        conta_id: 4477,
+        valor: 500
+      }
+    })
+    const output = response.data
+    expect(output.saldo).toBe(500)
+  })
+
+  test("Deve criar uma nova transacao com forma de pagamento PIX", async () => {
+    const response = await axios.post("http://localhost:3333/transacao", {
+      data: {
+        forma_pagamento: "P",
+        conta_id: 4477,
+        valor: 50
+      }
+    })
+    const output = response.data
+    expect(output.saldo).toBe(450)
+    expect(output.conta_id).toBe(4477)
+  })
 })
