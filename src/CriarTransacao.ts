b0fa4a95b1c6ef05853ca4c194ebf040a80b3561
Jairo Koning <jairo.koning@gmail.com>
2023-10-06 14:56:07 -0300

    feat: add usecase CriarTransacao

---
+++ src/CriarTransacao.ts
@@ -0,0 +1,17 @@
+import Conta from "./Conta";
+import ContaDAO from "./ContaDAO";
+import Transacao, { IFormaPagamento } from "./Transacao";
+import TransacaoDAO from "./TransacaoDAO";
+
+export default class CriarTransacao {
+  constructor(readonly transacaoDAO: TransacaoDAO, readonly contaDAO: ContaDAO) {}
+
+  async execute(forma_pagamento: IFormaPagamento, conta_id: number, valor: number) {
+    const transacao = Transacao.criar(forma_pagamento, conta_id, valor)
+    await this.transacaoDAO.criarTransacao(transacao)
+    const transacoes = await this.transacaoDAO.buscarTransacoes(conta_id)
+    const conta = await this.contaDAO.buscarConta(conta_id)
+    const saldo = Conta.calcularSaldo(conta!.valor!, transacoes)
+    return { conta_id, saldo }
+  }
+}
