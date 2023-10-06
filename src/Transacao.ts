fea2ebba3a8d16677641e1cd2ba0d93e19a97ef4
Jairo Koning <jairo.koning@gmail.com>
2023-10-06 14:46:48 -0300

    feat(transaction): add function that sum transactions value

--- src/Transacao.ts
+++ src/Transacao.ts
@@ -10,11 +10,18 @@ export default class Transacao {
   
   private constructor() {}
 
-  static create(forma_pagamento: IFormaPagamento, conta_id: number, valor: number) {
+  static criar(forma_pagamento: IFormaPagamento, conta_id: number, valor: number) {
     const transacao = new Transacao()
     transacao.forma_pagamento = forma_pagamento
     transacao.conta_id = conta_id
     transacao.valor = valor
     return transacao
+  }
+
+  static somarValor(transacoes: Transacao[]) {
+    const valorTotal = transacoes.reduce((total, transacao) => {
+      return transacao.valor
+    }, 0);
+    return valorTotal
   }
 }
