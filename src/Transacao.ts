246b36149cbbc110205631218eecdedfcc3c668c
Jairo Koning <jairo.koning@gmail.com>
2023-10-10 12:25:56 -0300

    feat(Transacao): implement data validation

--- src/Transacao.ts
+++ src/Transacao.ts
@@ -1,20 +1,16 @@
+import { BadRequestError } from "./BadRequestError"
 
-export interface IFormaPagamento {
+export type IFormaPagamento = {
   forma_pagamento: "P" | "C" | "D"
 }
 
-export default class Transacao {
-  forma_pagamento!: IFormaPagamento
-  conta_id!: number
-  valor!: number
-  
-  private constructor() {}
+export default class Transacao {  
+  private constructor(readonly forma_pagamento: IFormaPagamento, readonly conta_id: number, readonly valor: number) {
+    this.validarDados()    
+  }
 
-  static criar(forma_pagamento: IFormaPagamento, conta_id: number, valor: number) {
-    const transacao = new Transacao()
-    transacao.forma_pagamento = forma_pagamento
-    transacao.conta_id = conta_id
-    transacao.valor = valor
+  static criar(forma_pagamento: IFormaPagamento, conta_id: number, valor: number) {    
+    const transacao = new Transacao(forma_pagamento, conta_id, valor)    
     return transacao
   }
 
@@ -23,5 +19,15 @@ export default class Transacao {
       return total += transacao.valor
     }, 0);
     return valorTotal
+  }
+
+  private validarDados() {    
+    if (!Transacao.formaDePagamentoValido(this.forma_pagamento)) throw new BadRequestError("Forma de pagamento inválido")
+    if (this.valor < 0.1) throw new BadRequestError("Informe um valor acima de R$0,00")
+  }
+
+  static formaDePagamentoValido(forma_pagamento: any): forma_pagamento is IFormaPagamento {    
+    if (typeof forma_pagamento === 'string' && ['P', 'C', 'D'].includes(forma_pagamento)) return true    
+    return false
   }
 }
