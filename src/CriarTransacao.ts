a4adb09b672c1d1ae2d8810555c0901ae0cbd620
Jairo Koning <jairo.koning@gmail.com>
2023-10-10 12:27:02 -0300

    feat(CriarTransacao): implement data validation

--- src/CriarTransacao.ts
+++ src/CriarTransacao.ts
@@ -1,17 +1,33 @@
+import { AppError } from "./AppError";
 import Conta from "./Conta"
 import ContaDAO from "./ContaDAO";
+import ErrorHandler from "./ErrorHandler";
+import { NotFoundError } from "./NotFoundError";
 import Transacao, { IFormaPagamento } from "./Transacao";
 import TransacaoDAO from "./TransacaoDAO";
 
 export default class CriarTransacao {
-  constructor(readonly transacaoDAO: TransacaoDAO, readonly contaDAO: ContaDAO) {}
+  constructor(readonly transacaoDAO: TransacaoDAO, readonly contaDAO: ContaDAO, readonly appError: ErrorHandler) {}
 
   async execute(forma_pagamento: IFormaPagamento, conta_id: number, valor: number) {
+    const transacoes = await this.transacaoDAO.buscarTransacoes(conta_id)
+    const conta = await this.contaDAO.buscarConta(conta_id)    
+    if (!conta[0]) throw new NotFoundError("Conta não existe")
+    const saldo = Conta.calcularSaldo(conta[0].valor, transacoes)
+    if (valor > saldo) {
+      // throw new Error("Saldo insuficiente");
+      
+      //  ID: A1 =>  this works fine
+      // throw new AppError("Saldo insuficiente", 404);
+
+      this.appError.notFound("Saldo insuficiente")
+
+    
+      
+    }
+
     const transacao = Transacao.criar(forma_pagamento, conta_id, valor)
     await this.transacaoDAO.criarTransacao(transacao)
-    const transacoes = await this.transacaoDAO.buscarTransacoes(conta_id)
-    const conta = await this.contaDAO.buscarConta(conta_id)
-    const saldo = Conta.calcularSaldo(conta[0].valor, transacoes)
-    return { conta_id, saldo }
+    return { conta_id, saldo: saldo - valor }
   }
 }
