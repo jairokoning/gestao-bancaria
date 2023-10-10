59e3374bf15266274fb32324417391efe542c9f4
Jairo Koning <jairo.koning@gmail.com>
2023-10-10 12:40:26 -0300

    refactor: remove unnecessary code

--- src/CriarTransacao.ts
+++ src/CriarTransacao.ts
@@ -1,13 +1,11 @@
-import { AppError } from "./AppError";
 import Conta from "./Conta"
 import ContaDAO from "./ContaDAO";
-import ErrorHandler from "./ErrorHandler";
 import { NotFoundError } from "./NotFoundError";
 import Transacao, { IFormaPagamento } from "./Transacao";
 import TransacaoDAO from "./TransacaoDAO";
 
 export default class CriarTransacao {
-  constructor(readonly transacaoDAO: TransacaoDAO, readonly contaDAO: ContaDAO, readonly appError: ErrorHandler) {}
+  constructor(readonly transacaoDAO: TransacaoDAO, readonly contaDAO: ContaDAO) {}
 
   async execute(forma_pagamento: IFormaPagamento, conta_id: number, valor: number) {
     const transacoes = await this.transacaoDAO.buscarTransacoes(conta_id)
@@ -15,17 +13,8 @@ export default class CriarTransacao {
     if (!conta[0]) throw new NotFoundError("Conta não existe")
     const saldo = Conta.calcularSaldo(conta[0].valor, transacoes)
     if (valor > saldo) {
-      // throw new Error("Saldo insuficiente");
-      
-      //  ID: A1 =>  this works fine
-      // throw new AppError("Saldo insuficiente", 404);
-
-      this.appError.notFound("Saldo insuficiente")
-
-    
-      
+      throw new NotFoundError("Saldo insuficiente")      
     }
-
     const transacao = Transacao.criar(forma_pagamento, conta_id, valor)
     await this.transacaoDAO.criarTransacao(transacao)
     return { conta_id, saldo: saldo - valor }
--- tests/CriarTransacao.test.ts
+++ tests/CriarTransacao.test.ts
@@ -1,9 +1,7 @@
 import TransacaoDAOMemory from '../src/TransacaoDAOMemory'
 import ContaDAOMemory from '../src/ContaDAOMemory'
 import CriarTransacao from '../src/CriarTransacao'
-import Transacao, { IFormaPagamento } from '../src/Transacao'
-import AppErrorHandler from '../src/AppErrorHandler'
-import { AppError } from '../src/AppError'
+import { IFormaPagamento } from '../src/Transacao'
 import Conta from '../src/Conta'
 
 
@@ -12,9 +10,8 @@ describe("Test CriarTransacao usecase", () => {
   test("Deve criar uma nova transacao com forma de pagamento PIX", async () => {
     const contaDAO = new ContaDAOMemory()
     await contaDAO.criarConta({ conta_id: 1991, valor: 500 } as Conta)
-    const transacaoDAO = new TransacaoDAOMemory()
-    const appError = new AppErrorHandler();
-    const transacao = new CriarTransacao(transacaoDAO, contaDAO, appError)
+    const transacaoDAO = new TransacaoDAOMemory()    
+    const transacao = new CriarTransacao(transacaoDAO, contaDAO)
     const output = await transacao.execute("P" as unknown as IFormaPagamento, 1991, 25)
     expect(output.saldo).toBe(475)
     expect(output.conta_id).toBe(1991)
@@ -23,50 +20,18 @@ describe("Test CriarTransacao usecase", () => {
   test("Deve lançar uma exceção se não houver saldo", async () => {
     const contaDAO = new ContaDAOMemory()
     await contaDAO.criarConta({ conta_id: 1991, valor: 500 } as Conta)
-    const transacaoDAO = new TransacaoDAOMemory()
-    const appError = new AppErrorHandler();
-    const transacao = new CriarTransacao(transacaoDAO, contaDAO, appError)        
-    // expect(
-    //   transacao.execute("P" as unknown as IFormaPagamento, 1991, 501)
-    // ).rejects.toThrow(new Error("Saldo insuficiente"));
-
-    //  ID: A1 =>  this works fine
-    // expect(
-    //   transacao.execute("P" as unknown as IFormaPagamento, 1991, 501)
-    // ).rejects.toEqual(new AppError("Saldo insuficiente", 404));
-
+    const transacaoDAO = new TransacaoDAOMemory()    
+    const transacao = new CriarTransacao(transacaoDAO, contaDAO)            
     expect(
       transacao.execute("P" as unknown as IFormaPagamento, 1991, 501)
-    ).rejects.toEqual(new AppError("Saldo insuficiente", 404));
-  })
-
-  test.skip("teste teste", async() => {
-    const contaDAO = new ContaDAOMemory()
-    await contaDAO.criarConta({ conta_id: 1991, valor: 500 } as Conta)
-    const transacaoDAO = new TransacaoDAOMemory()
-    const appError = new AppErrorHandler();
-    const transacao = new CriarTransacao(transacaoDAO, contaDAO, appError)        
-    // expect(
-    //   transacao.execute("P" as unknown as IFormaPagamento, 1991, 501)
-    // ).rejects.toThrow(new Error("Saldo insuficiente"));
-
-    //  ID: A1 =>  this works fine
-    // expect(
-    //   transacao.execute("P" as unknown as IFormaPagamento, 1991, 501)
-    // ).rejects.toEqual(new AppError("Saldo insuficiente", 404));
-
-    expect(
-      transacao.execute("P" as unknown as IFormaPagamento, 1991, 0)
-    ).rejects.toThrow(new Error("Valor invalido"))
-    
+    ).rejects.toThrow(new Error("Saldo insuficiente"));
   })
 
   test("Deve lançar uma exceção se forma de pagamento inválido", async () => {
     const contaDAO = new ContaDAOMemory()
     await contaDAO.criarConta({ conta_id: 1991, valor: 500 } as Conta)
-    const transacaoDAO = new TransacaoDAOMemory()
-    const appError = new AppErrorHandler();
-    const transacao = new CriarTransacao(transacaoDAO, contaDAO, appError) 
+    const transacaoDAO = new TransacaoDAOMemory()    
+    const transacao = new CriarTransacao(transacaoDAO, contaDAO) 
     await expect(
       transacao.execute("X" as unknown as any, 1991, 75)
     ).rejects.toThrow(new Error("Forma de pagamento inválido"))
@@ -75,9 +40,8 @@ describe("Test CriarTransacao usecase", () => {
   test("Deve lançar uma exceção se valor da transação abaixo de 1 centavo", async () => {
     const contaDAO = new ContaDAOMemory()
     await contaDAO.criarConta({ conta_id: 1991, valor: 500 } as Conta)
-    const transacaoDAO = new TransacaoDAOMemory()
-    const appError = new AppErrorHandler();
-    const transacao = new CriarTransacao(transacaoDAO, contaDAO, appError) 
+    const transacaoDAO = new TransacaoDAOMemory()    
+    const transacao = new CriarTransacao(transacaoDAO, contaDAO) 
     await expect(
       transacao.execute("P" as any, 1991, 0)
     ).rejects.toThrow(new Error("Informe um valor acima de R$0,00"))
@@ -85,9 +49,8 @@ describe("Test CriarTransacao usecase", () => {
 
   test("Deve lançar uma exceção se a conta da transação não existir", async () => {
     const contaDAO = new ContaDAOMemory()    
-    const transacaoDAO = new TransacaoDAOMemory()
-    const appError = new AppErrorHandler();
-    const transacao = new CriarTransacao(transacaoDAO, contaDAO, appError) 
+    const transacaoDAO = new TransacaoDAOMemory()    
+    const transacao = new CriarTransacao(transacaoDAO, contaDAO) 
     await expect(
       transacao.execute("P" as any, 2002, 99.90)
     ).rejects.toThrow(new Error("Conta não existe"))
