1b4c6693027d1f929d471f917a7f94647e4f622f
Jairo Koning <jairo.koning@gmail.com>
2023-10-10 11:40:19 -0300

    feat(CriarConta): implement data validation

--- src/Conta.ts
+++ src/Conta.ts
@@ -1,20 +1,27 @@
+
+import { BadRequestError } from "./BadRequestError"
 import Transacao from "./Transacao"
 
 export default class Conta {
-  conta_id!: number
-  valor!: number
+  // conta_id!: number
+  // valor!: number
   
-  private constructor() {}
+  constructor(readonly conta_id: number, readonly valor: number) {
+    this.validarDados()
+  }
 
   static criar(conta_id: number, valor: number) {
-    const conta = new Conta()
-    conta.conta_id = conta_id
-    conta.valor = valor
+    const conta = new Conta(conta_id, valor)    
     return conta
   }
 
   static calcularSaldo(valorConta: number, transacoes: Transacao[]) {
     const valorTotalTransacoes = Transacao.somarValor(transacoes)    
     return valorConta - valorTotalTransacoes
+  }
+
+  validarDados() {
+    if (this.conta_id < 2) throw new BadRequestError("Número da conta inválido: conta_id")
+    if (this.valor < 100) throw new BadRequestError("Valor mínimo obrigatório: R$100")
   }
 }
--- src/CriarConta.ts
+++ src/CriarConta.ts
@@ -1,10 +1,13 @@
+import { ConflictError } from "./ConflictError";
 import Conta from "./Conta";
 import ContaDAO from "./ContaDAO";
 
 export default class CriarConta {
   constructor(readonly contaDAO: ContaDAO) {}
 
-  async execute(conta_id: number, valor: number) {    
+  async execute(conta_id: number, valor: number) {
+    const contaExiste = await this.contaDAO.buscarConta(conta_id)    
+    if (contaExiste[0]?.conta_id === conta_id) throw new ConflictError("Conta já cadastrada")
     const conta = Conta.criar(conta_id, valor)
     await this.contaDAO.criarConta(conta)    
     return { conta_id, saldo: valor }
