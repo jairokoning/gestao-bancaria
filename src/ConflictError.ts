d9027a7aca80be27f1b930528878e145c04cfd0f
Jairo Koning <jairo.koning@gmail.com>
2023-10-10 12:54:50 -0300

    feat: add custom error entity

---
+++ src/ConflictError.ts
@@ -0,0 +1,9 @@
+export class ConflictError extends Error {
+  statusCode?: number;
+
+  constructor (message: string) {
+    super(message)
+    this.name = 'ConflictError'
+    this.statusCode = 409
+  }
+}
