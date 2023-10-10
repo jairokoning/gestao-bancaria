277f2ff363f360be7ffaaea7ea7c97d8c049ab76
Jairo Koning <jairo.koning@gmail.com>
2023-10-10 11:37:18 -0300

    test: implement data validation tests

--- tests/Conta.test.ts
+++ tests/Conta.test.ts
@@ -1,7 +1,21 @@
 import Conta from "../src/Conta"
 
-test("Deve criar uma nova conta bancária", () => {
-  const conta = Conta.criar(1234, 200)
-  expect(conta.valor).toBe(200)
-  expect(conta.conta_id).toBe(1234)
+describe("Conta | Testes Unitários", () => {
+  test("Deve criar uma nova conta bancária", () => {
+    const conta = Conta.criar(1234, 500)
+    expect(conta.valor).toBe(500)
+    expect(conta.conta_id).toBe(1234)
+  })
+
+  test.skip("Deve validar o numero da conta", () => {  
+    expect(      
+      Conta.criar(0, 100)
+    ).rejects.toThrow("Número inválido: conta_id")  
+  })
+
+  test.skip("Deve validar o valor de entrada", () => {  
+    expect(
+      Conta.criar(8822, 90)
+    ).rejects.toEqual(new Error("Valor mínimo obrigatório: R$100"))  
+  })
 })
