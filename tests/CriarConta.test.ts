ca1b3102e4b6f97eee73f4a7a2f6437bbf84c7de
Jairo Koning <jairo.koning@gmail.com>
2023-10-10 11:40:44 -0300

    test(CriarConta): implement data validation tests

--- tests/CriarConta.test.ts
+++ tests/CriarConta.test.ts
@@ -1,10 +1,23 @@
 import CriarConta from '../src/CriarConta'
 import ContaDAOMemory from '../src/ContaDAOMemory'
+import Conta from '../src/Conta'
 
-test("Deve criar uma nova conta bancária com saldo inicial de R$500", async () => {
-  const contaDAO = new ContaDAOMemory()
-  const criarConta = new CriarConta(contaDAO)
-  const output = await criarConta.execute(9876, 500)
-  expect(output.saldo).toBe(500)
-  expect(output.conta_id).toBe(9876)
+describe("CriarConta | Testes de integração (usecase)", () => {
+  test("Deve criar uma nova conta bancária com saldo inicial de R$500", async () => {
+    const contaDAO = new ContaDAOMemory()
+    const criarConta = new CriarConta(contaDAO)
+    const output = await criarConta.execute(9876, 500)
+    expect(output.saldo).toBe(500)
+    expect(output.conta_id).toBe(9876)
+  })
+
+  test("Deve lançar uma exceção se a conta já existir", async () => {
+    const contaDAO = new ContaDAOMemory()
+    const criarConta = new CriarConta(contaDAO)
+    await contaDAO.criarConta({ conta_id: 9876, valor: 970 } as Conta)
+    await expect(
+      criarConta.execute(9876, 500)
+    ).rejects.toThrow(new Error("Conta já cadastrada"))
+  })
+
 })
